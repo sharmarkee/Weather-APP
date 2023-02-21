c0c0b8381eb9df40182a2decdd9a64c229afd7a8
sharmarkee <96698799+sharmarkee@users.noreply.github.com>
2023-02-21 03:55:46 -0600

    Add files via upload
    Weather-APP using React.JS

---
+++ api.js
@@ -0,0 +1,10 @@
+ export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6503e0281bmshf73d4a34b58447cp1bae28jsn670c5947a538',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
